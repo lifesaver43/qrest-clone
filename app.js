cc505c29acc7313699865a9c0f9d16987d17298d
lifesaver43 <innocentnwafor17@gmail.com>
2022-10-27 18:15:45 +0100

    second

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 window.addEventListener("scroll", () => {
     document.querySelector("nav").classList.toggle("window-scrolled", window.scrollY > 0);
 })
+
+// features accordion
+const list = document.getElementsByClassName("list");
+
+for(i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", function(){
+        this.classList.toggle("active")
+    })
+}
